9cea3a4a4c0743a1b47b9aeac59d4caf168e3676
Filip Poboril <fpoboril@redhat.com>
2021-02-18 17:45:46 +0100

    Base connexion app + add tox

---
+++ src/rhub/api/health.py
@@ -0,0 +1,18 @@
+def ping():
+    """Always returns 'pong'."""
+    return 'pong'
+
+
+def cowsay():
+    return r"""
+      ___________________
+    < Hello Resource Hub! >
+      ===================
+                            \
+                             \
+                               ^__^
+                               (oo)\_______
+                               (__)\       )\/\
+                                   ||----w |
+                                   ||     ||
+    """
