319bf34d8bf784275375d0db4a9f79d86715d38c
Jan Dzurek <jdzurek@redhat.com>
2021-02-25 07:11:47 +0100

    add CORS support

--- src/rhub/api/__init__.py
+++ src/rhub/api/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 import connexion
+from flask_cors import CORS
 
 import rhub
 
@@ -10,3 +11,5 @@ ROOT = os.path.dirname(rhub.__path__[0])
 
 app = connexion.FlaskApp(__name__, specification_dir=os.path.join(ROOT, 'openapi'))
 app.add_api('openapi.yml')
+
+CORS(app.app)
