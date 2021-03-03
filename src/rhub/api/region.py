737a5f14ca88df1dd9bb0c8097ae9c10ba7a7cf4
Filip Poboril <fpoboril@redhat.com>
2021-03-03 15:33:30 +0100

    Add dummy handlers

---
+++ src/rhub/api/region.py
@@ -0,0 +1,30 @@
+def list_regions():
+    raise NotImplementedError
+
+
+def create_region(body):
+    raise NotImplementedError
+
+
+def get_region(id):
+    raise NotImplementedError
+
+
+def update_region(id, body):
+    raise NotImplementedError
+
+
+def delete_region(id):
+    raise NotImplementedError
+
+
+def list_region_templates(id):
+    raise NotImplementedError
+
+
+def add_region_template(id, body):
+    raise NotImplementedError
+
+
+def delete_region_template(id, body):
+    raise NotImplementedError
