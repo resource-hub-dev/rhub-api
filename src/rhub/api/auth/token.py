8750f4098899ecdae6c85a98d8bce8688cb9f0b2
Filip Poboril <fpoboril@redhat.com>
2021-03-30 12:49:27 +0200

    Add auth API spec

---
+++ src/rhub/api/auth/token.py
@@ -0,0 +1,9 @@
+def get_token_info():
+    ...
+
+    # See: RFC 7662, Section 2.2
+    return {
+        'active': True,
+        'scope': 'user',
+        'sub': 1,
+    }
---
+++ src/rhub/api/auth/user.py
@@ -0,0 +1,46 @@
+def list_users():
+    raise NotImplementedError
+
+
+def create_user(body):
+    raise NotImplementedError
+
+
+def get_user(id):
+    raise NotImplementedError
+
+
+def update_user(id, body):
+    raise NotImplementedError
+
+
+def delete_user(id):
+    raise NotImplementedError
+
+
+def list_user_groups(id):
+    raise NotImplementedError
+
+
+def add_user_group(id, body):
+    raise NotImplementedError
+
+
+def delete_user_group(id):
+    raise NotImplementedError
+
+
+def list_user_roles(id):
+    raise NotImplementedError
+
+
+def add_user_role(id, body):
+    raise NotImplementedError
+
+
+def delete_user_role(id, body):
+    raise NotImplementedError
+
+
+def get_current_user():
+    raise NotImplementedError
