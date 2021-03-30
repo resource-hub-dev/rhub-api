fb9cefe1615587cb2f946b14194f3aa4245133f9
Filip Poboril <fpoboril@redhat.com>
2021-03-30 12:49:27 +0200

    Add auth API spec

---
+++ src/rhub/api/auth/group.py
@@ -0,0 +1,34 @@
+def list_groups():
+    raise NotImplementedError
+
+
+def create_group(body):
+    raise NotImplementedError
+
+
+def get_group(id):
+    raise NotImplementedError
+
+
+def update_group(id, body):
+    raise NotImplementedError
+
+
+def delete_group(id):
+    raise NotImplementedError
+
+
+def list_group_users(id):
+    raise NotImplementedError
+
+
+def list_group_roles(id):
+    raise NotImplementedError
+
+
+def add_group_role(id, body):
+    raise NotImplementedError
+
+
+def delete_group_role(id, body):
+    raise NotImplementedError
---
+++ src/rhub/api/auth/role.py
@@ -0,0 +1,18 @@
+def list_roles():
+    raise NotImplementedError
+
+
+def create_role(body):
+    raise NotImplementedError
+
+
+def get_role(id):
+    raise NotImplementedError
+
+
+def update_role(id, body):
+    raise NotImplementedError
+
+
+def delete_role(id):
+    raise NotImplementedError
