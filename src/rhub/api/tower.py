546c5287e6bdcc08591af7e1c6471af2b1fee0ea
Filip Poboril <fpoboril@redhat.com>
2021-04-26 12:02:43 +0200

    Add API spec for Tower module

---
+++ src/rhub/api/tower.py
@@ -0,0 +1,58 @@
+def list_servers():
+    raise NotImplementedError
+
+
+def create_server(body):
+    raise NotImplementedError
+
+
+def get_server(id):
+    raise NotImplementedError
+
+
+def update_server(id, body):
+    raise NotImplementedError
+
+
+def delete_server(id):
+    raise NotImplementedError
+
+
+def list_templates():
+    raise NotImplementedError
+
+
+def create_template(body):
+    raise NotImplementedError
+
+
+def get_template(id):
+    raise NotImplementedError
+
+
+def update_template(id, body):
+    raise NotImplementedError
+
+
+def delete_template(id):
+    raise NotImplementedError
+
+
+def launch_template(id, body):
+    raise NotImplementedError
+
+
+def list_jobs():
+    raise NotImplementedError
+
+
+def get_job(id):
+    raise NotImplementedError
+
+
+def relaunch_job(id):
+    raise NotImplementedError
+
+
+def get_job_stdout(id):
+    raise NotImplementedError
