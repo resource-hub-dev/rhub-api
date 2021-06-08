f10c44af7beda8dcab38188b3aabe74e0b565aa0
Filip Poboril <fpoboril@redhat.com>
2021-06-08 13:40:28 +0200

    Add lab model

--- src/rhub/api/utils.py
+++ src/rhub/api/utils.py
@@ -1,5 +1,7 @@
 def row2dict(row):
     """Convert SQLAlchemy row to dict."""
+    if hasattr(row, 'to_dict'):
+        return row.to_dict()
     d = {}
     for column in row.__table__.columns:
         d[column.name] = getattr(row, column.name)
---
+++ src/rhub/lab/model.py
@@ -0,0 +1,130 @@
+import datetime
+
+from sqlalchemy.dialects import postgresql
+
+from rhub.api import db, get_vault
+from rhub.api.utils import row2dict
+from rhub.tower.client import Tower
+
+
+class Region(db.Model):
+    __tablename__ = 'lab_region'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True, nullable=False)
+    location = db.Column(db.String(32), index=True, nullable=True)
+    description = db.Column(db.Text, nullable=False, default='')
+    banner = db.Column(db.Text, nullable=False, default='')
+    enabled = db.Column(db.Boolean, default=True)
+    quota_id = db.Column(db.Integer, db.ForeignKey('lab_quota.id'),
+                         nullable=True)
+    quota = db.relationship('Quota', uselist=False, back_populates='region')
+    lifespan_length = db.Column(db.Integer, nullable=True)
+    reservations_enabled = db.Column(db.Boolean, default=True)
+    owner_group = db.Column(postgresql.UUID, nullable=False)
+    # Limits use only to specific group of people. NULL == shared lab.
+    users_group = db.Column(postgresql.UUID, nullable=True, index=True)
+    ...  # TODO policies?
+    tower_id = db.Column(db.Integer, db.ForeignKey('lab_tower.id'))
+    tower = db.relationship('Tower')
+
+    clusters = db.relationship('Cluster', back_populates='region')
+
+    def create_cluster(self, **kwargs):
+        cluster = Cluster(**kwargs)
+
+        lifespan_delta = datetime.timedelta(days=self.lifespan_length)
+        cluster.lifespan_expiration = cluster.created + lifespan_delta
+
+        return cluster
+
+    def to_dict(self):
+        data = {}
+        for column in self.__table__.columns:
+            if column.name == 'quota_id':
+                continue
+            data[column.name] = getattr(self, column.name)
+
+        if self.quota:
+            data['quota'] = row2dict(self.quota)
+        else:
+            data['quota'] = None
+
+        return data
+
+
+class Quota(db.Model):
+    __tablename__ = 'lab_quota'
+
+    id = db.Column(db.Integer, primary_key=True)
+    num_vcpus = db.Column(db.Integer, nullable=True)
+    ram_mb = db.Column(db.Integer, nullable=True)
+    num_volumes = db.Column(db.Integer, nullable=True)
+    volumes_gb = db.Column(db.Integer, nullable=True)
+
+    region = db.relationship('Region', back_populates='quota',
+                             cascade='all,delete-orphan')
+
+    def to_dict(self):
+        data = {}
+        for column in self.__table__.columns:
+            if column.name == 'id':
+                continue
+            data[column.name] = getattr(self, column.name)
+        return data
+
+
+class Tower(db.Model):
+    __tablename__ = 'lab_tower'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    enabled = db.Column(db.Boolean, default=True)
+    url = db.Column(db.String(256), nullable=False)
+    credentials = db.Column(db.String(256), nullable=False)
+
+    regions = db.relationship('Region', back_populates='tower',
+                              cascade='all,delete-orphan')
+
+    def create_tower_client(self) -> Tower:
+        """
+        Create Tower client.
+
+        Returns: Tower
+        Raises: RuntimeError if failed to create client due to missing
+                credentials in vault
+        Raises: Exception any other errors
+        """
+        credentials = get_vault().read(self.credentials)
+        if not credentials:
+            raise RuntimeError('Missing credentials in vault')
+        return Tower(
+            url=self.url,
+            username=credentials['username'],
+            password=credentials['password'],
+        )
+
+
+class Cluster(db.Model):
+    __tablename__ = 'lab_cluster'
+
+    id = db.Column(db.Integer, primary_key=True)
+    region_id = db.Column(db.Integer, db.ForeignKey('lab_region.id'),
+                          nullable=False)
+    region = db.relationship('Region', back_populates='clusters')
+
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    reservation_expiration = db.Column(db.DateTime, nullable=True)
+    lifespan_expiration = db.Column(db.DateTime, nullable=True)
+
+    ...  # TODO
+
+    @property
+    def quota(self):
+        return self.region.quota
+
+    def to_dict(self):
+        data = row2dict(self)
+        data['quota'] = self.quota
+        return data
