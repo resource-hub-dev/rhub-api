60318d0eb1411efa667f5a6f7d09f9f4ff184306
Filip Poboril <fpoboril@redhat.com>
2021-09-07 12:28:26 +0200

    Add cronjob to launch tower template

--- src/rhub/scheduler/jobs.py
+++ src/rhub/scheduler/jobs.py
@@ -1,5 +1,7 @@
 import logging
 
+import rhub.tower.model
+
 
 logger = logging.getLogger(__name__)
 
@@ -38,3 +40,37 @@ class CronJob:
 def example(params):
     """Example cron job."""
     logger.info(f'Executing example cron job, {params=}')
+
+
+@CronJob
+def tower_launch(params):
+    """
+    Launch template in Tower.
+
+    params:
+        tower_id -- ID of the Tower server in tower module
+            (:class:`rhub.tower.model.Server`)
+        template_id -- ID of the template in the Tower
+        template_is_workflow -- bool if the template in Tower is workflow
+            template, optional, default is `False`
+        extra_vars -- dict of extra variables to pass to template, optiona,
+            default is empty dict
+    """
+    server = rhub.tower.model.Server.query.get(params['tower_id'])
+    client = server.create_tower_client()
+
+    template_id = params['template_id']
+    extra_vars = params.get('extra_vars', {})
+
+    if params.get('template_is_workflow', False):
+        launch = client.workflow_launch
+        template_type = 'workflow template'
+    else:
+        launch = client.template_launch
+        template_type = 'template'
+
+    job_data = launch(template_id, extra_vars)
+    logger.info(
+        f'Launched {template_type} {template_id} in Tower {server.name} '
+        f'(ID: {server.id}), job ID in Tower: {job_data["id"]}'
+    )
