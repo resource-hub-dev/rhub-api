b1dc8a6cbd105520515c82825e61597feef0ca33
Filip Poboril <fpoboril@redhat.com>
2022-02-10 13:57:45 +0100

    Fix DB deadlock in setup script

--- src/rhub/api/_setup.py
+++ src/rhub/api/_setup.py
@@ -18,10 +18,11 @@ logger = logging.getLogger(__name__)
 
 # This function must be idempotent. In container, it may be called on every
 # start.
+# Keycloak setup is in a separate function because we need retry logic on it
+# (Keycloak is slow on start and may be unavailable for the first few seconds),
+# but retry causes a deadlock in DB, if used for DB setup and something failed.
 @tenacity.retry(wait=tenacity.wait_fixed(5000), stop=tenacity.stop_after_attempt(3))
-def setup():
-    db.create_all()
-
+def setup_keycloak():
     keycloak = di.get(KeycloakClient)
 
     groups = {group['name']: group for group in keycloak.group_list()}
@@ -94,6 +95,14 @@ def setup():
                     f'to sharedcluster group "{SHAREDCLUSTER_GROUP}"')
         keycloak.group_user_add(sharedcluster_user['id'],
                                 groups[SHAREDCLUSTER_GROUP]['id'])
+
+
+# This function must be idempotent. In container, it may be called on every
+# start.
+def setup():
+    db.create_all()
+
+    setup_keycloak()
 
     query = scheduler_model.SchedulerCronJob.query.filter(
         scheduler_model.SchedulerCronJob.job_name
