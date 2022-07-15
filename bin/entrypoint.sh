f6d2e04114a6feb4cff0f439f438ac7b1b89ff87
Fernando Reed <freed@redhat.com>
2022-07-15 15:23:19 +0000

    Improved OTEL config generation program.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+sleep 10
+
 if [[ $RHUB_SKIP_INIT != "True" ]]; then
     echo 'Calling flask initialization...'
     flask init
--- bin/gen_otel_config.py
+++ bin/gen_otel_config.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python3
 
-import requests
+# This program writes configs for OTEL so we can start that with
+# the right list of exporters for Prometheus-like federation.
 import os
-import urllib3
 import json
 
-from pathlib import Path
+import requests
+import urllib3
 
-from pdb import set_trace as stp
 
-DEBUG = False
-if "DEBUG" in os.environ:
-    if os.environ["DEBUG"].lower() in ["yes", "on", "true"]:
-        DEBUG = True
+def load_config():
+    r = {}
 
-def write_hosts_file(filename, jobname, uri, token):
+    DEBUG = False
+    if "DEBUG" in os.environ:
+        if os.environ["DEBUG"].lower() in ["yes", "on", "true"]:
+            DEBUG = True
+
+    if DEBUG:
+        urllib3.disable_warnings()
+        hostname = "http://rhub-api"
+        if "RHUB_API_HOSTNAME" in os.environ:
+            hostname = os.environ["RHUB_API_HOSTNAME"]
+        port = 8081
+        auth = ("admin2", "admin2")
+    else:
+        hostname = os.environ["RHUB_API_HOSTNAME"]
+        port = os.environ["RHUB_API_PORT"]
+        auth_user = os.environ["RHUB_API_USER"]
+        auth_pwd = os.environ["RHUB_API_PWD"]
+        auth = (auth_user, auth_pwd)
+
+    script_dir = os.path.abspath(os.path.dirname(__file__))
+    # one level up from current path
+    parent_dir = "/".join(script_dir.split("/")[:-1])
+    config_file_dir = f"{parent_dir}/config"
+
+    r["config_file_dir"] = config_file_dir
+    r["auth"] = auth
+    r["hostname"] = hostname
+    r["path"] = '/v0'
+    r["port"] = port
+    r["DEBUG"] = DEBUG
+
+    return r
+
+
+def write_hosts_file(config, filename, jobname, uri, token):
     print(f"Writing job: {jobname}/{filename}.json...")
-    hostlist = call_api(uri, token)
+    hostlist = call_api(config, uri, token)
     data = hostlist["data"]
     all_hosts = []
     for host_info in data:
@@ -31,33 +63,24 @@ def write_hosts_file(filename, jobname, uri, token):
     }
     out_content = [out_h]
 
+    config_file_dir = config["config_file_dir"]
+
     config_file_nodes = f"{config_file_dir}/{filename}.json"
     with open(config_file_nodes, "w") as f:
         json.dump(out_content, f)
 
-script_dir = os.path.abspath(os.path.dirname(__file__))
-parent_dir = "/".join(script_dir.split("/")[:-1])
-config_file_dir = f"{parent_dir}/config"
 
-PATH = '/v0'
+def call_api(config, uri, token=None):
+    DEBUG = config["DEBUG"]
+    hostname = config["hostname"]
+    port = config["port"]
+    path = config["path"]
+    auth = config["auth"]
 
-HOSTNAME = "https://not.defined.local"
-PORT = 443
+    url = f"{hostname}:{port}{path}/{uri}"
+    print(f"calling rl {url}")
 
-if DEBUG:
-    urllib3.disable_warnings()
-    HOSTNAME = "http://10.0.1.30"
-    PORT = 8081
-    auth = ("admin2", "admin2")
-else:
-    auth_user = os.environ["USER"]
-    auth_pwd = os.environ["PWD"]
-    auth = (auth_user, auth_pwd)
-
-def call_api(uri, token=None):
-    url = f"{HOSTNAME}:{PORT}{PATH}/{uri}"
-    print(f"calling url {url}")
-    if None == token:
+    if token is None:
         stage_one = requests.post(url, auth=auth, timeout=2, verify=False)
     else:
         stage_one = requests.get(
@@ -70,15 +93,30 @@ def call_api(uri, token=None):
     if DEBUG:
         print(stage_one)
 
-    stage_two = json.loads(stage_one.content.decode("utf-8").replace("'", '"'))
+    decoded = stage_one.content.decode("utf-8").replace("'", '"')
+    stage_two = json.loads(decoded)
     return stage_two
 
-token_call = call_api("/auth/token/create")
-if DEBUG:
-    print(token_call)
 
-real_token = token_call["access_token"]
+def main():
+    config = load_config()
+    DEBUG = config["DEBUG"]
+
+    token_call = call_api(config, "/auth/token/create")
+    if DEBUG:
+        print(token_call)
+
+    real_token = token_call["access_token"]
+
+    write_hosts_file(config,
+                     "otel-all-nodes",
+                     "all-nodes",
+                     "/monitor/bm/hosts/node", real_token)
+    write_hosts_file(config,
+                     "otel-internal-rhub-exporter",
+                     "internal-rhub-exporter",
+                     "/monitor/bm/hosts/app", real_token)
 
 
-write_hosts_file("otel-all-nodes", "all-nodes", "/monitor/bm/hosts/node", real_token)
-write_hosts_file("otel-internal-rhub-exporter", "internal-rhub-exporter", "/monitor/bm/hosts/app", real_token)
+if __name__ == "__main__":
+    main()
