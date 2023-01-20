6f3c8210308c5b720f7f9828547dc754da6b5528
Filip Poboril <fpoboril@redhat.com>
2023-01-20 13:09:14 +0100

    Cleanup groups

--- src/rhub/auth/tasks.py
+++ src/rhub/auth/tasks.py
@@ -1,8 +1,11 @@
 from rhub.api import db, di
-from rhub.auth import model
+from rhub.auth import ADMIN_GROUP, model
 from rhub.auth.ldap import LdapClient
+from rhub.dns import model as dns_model
+from rhub.lab import model as lab_model
 from rhub.messaging import Messaging
 from rhub.openstack import model as openstack_model
+from rhub.satellite import model as satellite_model
 
 
 def update_users():
@@ -55,4 +58,63 @@ def cleanup_users():
 
         user.deleted = True
 
+        db.session.commit()
+
+
+def cleanup_groups():
+    ldap_client = di.get(LdapClient)
+
+    admin_group = model.Group.query.filter(model.Group.name == ADMIN_GROUP).first()
+
+    groups_query = model.Group.query.filter(model.Group.ldap_dn.isnot(None))
+    for group in groups_query:
+        ldap_query = ldap_client.get(group.ldap_dn)
+        if len(ldap_query) > 0:
+            continue
+
+        lab_model.Region.query.filter(
+            lab_model.Region.users_group_id == group.id,
+        ).update(
+            {'users_group_id': None},
+            synchronize_session='fetch',
+        )
+
+        lab_model.Region.query.filter(
+            lab_model.Region.owner_group_id == group.id
+        ).update(
+            {'owner_group_id': admin_group.id},
+            synchronize_session='fetch',
+        )
+
+        openstack_model.Project.query.filter(
+            openstack_model.Project.group_id == group.id,
+        ).update(
+            {'group_id': None},
+            synchronize_session='fetch',
+        )
+
+        openstack_model.Cloud.query.filter(
+            openstack_model.Cloud.owner_group_id == group.id
+        ).update(
+            {'owner_group_id': admin_group.id},
+            synchronize_session='fetch',
+        )
+
+        satellite_model.SatelliteServer.query.filter(
+            satellite_model.SatelliteServer.owner_group_id == group.id
+        ).update(
+            {'owner_group_id': admin_group.id},
+            synchronize_session='fetch',
+        )
+
+        dns_model.DnsServer.query.filter(
+            dns_model.DnsServer.owner_group_id == group.id
+        ).update(
+            {'owner_group_id': admin_group.id},
+            synchronize_session='fetch',
+        )
+
+        model.UserGroup.query.filter(model.UserGroup.group_id == group.id).delete()
+
+        db.session.delete(group)
         db.session.commit()
